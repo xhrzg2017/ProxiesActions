fc13d362ab26798a995d2b1e63112c80476ee6df
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2022-03-10 23:16:15 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import urllib.request
+import ssl
+context = ssl._create_unverified_context()
+
+#1.数据url
+url = 'https://sub.789.st/sub?target=clash&new_name=true&url=https://hk.xhrzg2017.xyz/clash/proxies?nc=CN&type=vmess,trojan&config=https://raw.githubusercontent.com/xhrzg2017/listes/main/Clash/Config/low.ini&emoji=true&list=false&udp=false&tfo=false&scv=false&fdn=false&sort=false'
+#2.添加请求头
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+}
+#3.构建请求对象
+request = urllib.request.Request(url, headers=headers)  #使用Request可以加请求头对象
+#4.发送请求对象
+response = urllib.request.urlopen(request,context=context)
+#5.读取数据
+data = response.read()
+#print(data)
+#6.保存到文件中 验证数据
+with open('clash.yaml', 'wb') as f:
+    f.write(data)
